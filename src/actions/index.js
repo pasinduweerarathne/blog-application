56c23102321fce5c7b95df2bbfa99f400c929640
H.K.D.Pasindu Pramod Weerarathne <pasiweerarathne98@gmail.com>
2022-05-18 01:00:47 +0530

    modify the code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+// import _ from "lodash";
+import _ from "lodash";
 import jsonPlaceholder from "../api/jsonPlaceholder";
 
 /* export const fetchPosts = () => {
@@ -15,8 +17,39 @@ import jsonPlaceholder from "../api/jsonPlaceholder";
 // making a request from an action creator
 // if we use redux thunk we need to return a function. that's why we use a function inside action creator
 // we go through this middleware instead of send action dirrectly to reducers.
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  /* const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach((id) => dispatch(fetchUser(id))); */
+  // chain method
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
+};
+
 export const fetchPosts = () => async (dispatch, getState) => {
   const response = await jsonPlaceholder.get("/posts");
 
-  dispatch({ type: "FETCH_POSTS", payload: response });
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
+
+export const fetchUser = (id) => async (dispatch, getState) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USERS", payload: response.data });
+};
+
+/* // fetch users
+export const fetchUser = (id) => (dispatch, getState) => {
+  _fetchUser(id, dispatch);
+}; */
+
+/* // need to memorize the function bcz of to prevent unnecessary network requests (one time memorization)
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USERS", payload: response.data });
+}); */
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 // import all the reducers that we created in the project
 import { combineReducers } from "redux";
 import postReducer from "./postReducer";
+import userReducer from "./userReducer";
 
 export default combineReducers({
   posts: postReducer,
+  user: userReducer,
 });
